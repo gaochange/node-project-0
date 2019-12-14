fc95fb7bb83116001a52d3b5488e5cc7dcc4f843
gaochange <45093419+gaochange@users.noreply.github.com>
2019-12-15 00:26:38 +0800

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -7,10 +7,11 @@ http.createServer(function (request, response) {
         // 发送 HTTP 头部 
         // HTTP 状态值: 200 : OK
         // 内容类型: text/plain
-        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.writeHead(200, {'Content-Type': 'text/html';charset=utf-8'});
 
         // 发送响应数据 "Hello World"
-        response.end('wdnmd niuzhiming\n');
+        response.end('<p>我打尼玛的 牛志敏
+                                 <img src="/root/b/1.jpg" width="128" height="128">'');
 }).listen(port,host);
 
 // 终端打印如下信息
